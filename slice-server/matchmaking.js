ee3076b7dcaeb698ec09af6fe49605ff0d049343
JetDesktop <jethro0610@outlook.com>
2022-01-08 17:54:11 -0800

    Relog the headers

--- slice-server/matchmaking.js
+++ slice-server/matchmaking.js
@@ -34,6 +34,7 @@ const initSocketIO = (http, corsOptions) => {
             idSocket.delete(socket.peerId);
             searchingClients.delete(socket);
         });
+        console.log(socket.request.headers);
         const socketIP = socket.request.headers['X-Forwarded-For'];
         console.log(socketIP);
         console.log(geoip.lookup(socketIP));
